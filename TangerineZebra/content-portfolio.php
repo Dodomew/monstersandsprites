af479a992f22b4e04e90369a75cc9b3e342ce2b3
Doortje Spanjerberg <dodomew@gmail.com>
2016-10-25 14:35:43 +0200

    styled footer, fixed featured image when not available and started portfolio template styling

---
+++ TangerineZebra/content-portfolio.php
@@ -0,0 +1,22 @@
+<div class="content-portfolio-container">
+    
+    <div class="blog-square">
+
+        <div class="blog-post">
+
+            <h2 class="blog-post-title"><?php the_title(); ?></h2>
+
+            <!--p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>-->
+
+            <?php if ( has_post_thumbnail() ) 
+            {
+                the_post_thumbnail();
+            } ?>
+
+         <?php the_content(); ?>
+
+        </div><!-- /.blog-post -->
+        
+    </div><!--.blog-square-->
+    
+</div>
